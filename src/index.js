0fd7e8e76d7920b1ef8f68b34d221e37afb40739
Yaro Celis <yaro360@gmail.com>
2025-05-13 15:29:03 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IPOTrackerApp from './IPOTracker';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <IPOTrackerApp />
+  </React.StrictMode>
+);
